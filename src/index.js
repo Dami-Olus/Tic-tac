f594f2fdd005669c3811831b59243b69ca93d736
Damola Olusakin <olusakinadedamola@gmail.com>
2020-11-21 15:06:50 +0100

    take turns

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import './index.css';
 
 
 function Square(props) {
-    
-    
       return (
         <button 
         className="square" 
-        onClick ={() => props.onClick()} >
+        onClick ={props.onClick} >
           {props.value}
         </button>
       );
@@ -23,27 +21,29 @@ function Square(props) {
           super(props);
           this.state = {
               squares: Array(9).fill(null),
+              xIsNext: true,
           };
           
       }
 
       handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i]='X';
+        squares[i]= this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares:squares,
+            xIsNext: !this.state.xIsNext,
         });
     }      
 
     renderSquare(i) {
-      return <Square 
+      return (<Square 
                 value={this.state.squares[i]}
                 onClick = {()=>this.handleClick(i)}
-      />;
+      />);
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
@@ -53,7 +53,7 @@ function Square(props) {
             {this.renderSquare(1)}
             {this.renderSquare(2)}
           </div>
-          <div className="board-row">
+          <div className=" board-row">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
